d0adf9cb7068de681b94d9008bfcb0d50ec9b1d0
李鑫 <lixin544@163.com>
2018-01-24 00:35:38 +0800

    put GeminiScrollbar on Vue.$geminiScrollbar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import GeminiScrollbar from './geminiScrollbar.vue'
+import GS from 'gemini-scrollbar'
 
 const install = function (Vue) {
   Vue.component(GeminiScrollbar.name, GeminiScrollbar)
+  Vue.prototype.$geminiScrollbar = Vue.$geminiScrollbar = GS
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
-};
+  window.Vue.prototype.$geminiScrollbar = window.Vue.$geminiScrollbar = GS
+}
 
 export default {
   install,
